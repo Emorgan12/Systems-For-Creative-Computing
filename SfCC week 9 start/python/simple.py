be89fd7ef589a4020c82cea96173bddfc78316bd
Elliot <140021772+Emorgan12@users.noreply.github.com>
2025-11-27 16:45:39 +0000

    Undo Week 9

--- SfCC week 9 start/python/simple.py
+++
@@ -1,6 +0,0 @@
-from pythonosc.udp_client import SimpleUDPClient
-sc = SimpleUDPClient("127.0.0.1", 57120)
-
-sc.send_message("/amen", [])
-sc.send_message("/volume", [0.5])
-sc.send_message("/speed", [1.2])
